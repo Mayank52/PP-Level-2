e34dc6756e4db6197f8b22ca30efc0ca5515a488
Mayank52 <mayank.aggarwal963@gmail.com>
2021-04-13 19:02:34 +0530

    update

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 // 925. Long Pressed Name
 /*
+Approach: 2 Pointer
 Time Complexity: O(n+m), Space: O(1)
 
 To be Long Pressed:
@@ -62,6 +64,140 @@ bool isLongPressedName(string name, string typed)
     return true;
 }
 
+// 11. Container With Most Water
+/*
+Approach: 2 Pointer
+Time: O(n)
+*/
+int maxArea(vector<int> &height)
+{
+    int n = height.size();
+
+    int maxWater = 0, i = 0, j = n - 1;
+    while (i < j)
+    {
+        //length(j-i) * min height
+        maxWater = max(maxWater, (j - i) * min(height[i], height[j]));
+
+        if (height[i] < height[j])
+            i++;
+        else
+            j--;
+    }
+
+    return maxWater;
+}
+
+// 977. Squares of a Sorted Array
+/*
+Approach: 2 Pointer
+Time: O(n)
+
+Approach: 
+Since array is sorted, so the max square will either be of the max element or the min element(in case of -ve numbers)
+So, We use 2 pointers, start and end
+at each step we find the max element between both elements and place it at the end of result array
+*/
+vector<int> sortedSquares(vector<int> &nums)
+{
+    int n = nums.size();
+
+    vector<int> res(n);
+    int i = 0, j = n - 1, idx = n - 1;
+    while (i <= j)
+    {
+        if (abs(nums[i]) > abs(nums[j]))
+        {
+            res[idx] = nums[i] * nums[i];
+            i++;
+        }
+        else
+        {
+            res[idx] = nums[j] * nums[j];
+            j--;
+        }
+        idx--;
+    }
+
+    return res;
+}
+
+// 169. Majority Element (Boyer Moore Voting Algo)
+int majorityElement(vector<int> &nums)
+{
+    int n = nums.size();
+    int cand = nums[0], count = 1;
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] == cand)
+            count++;
+        else
+            count--;
+
+        if (count == 0)
+        {
+            cand = nums[i];
+            count = 1;
+        }
+    }
+
+    count = 0;
+    for (int i = 0; i < nums.size(); i++)
+        if (cand == nums[i])
+            count++;
+
+    return count > n / 2 ? cand : -1;
+}
+
+// 229. Majority Element II
+vector<int> majorityElement(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int cand1 = 0, cand2 = 0, count1 = 0, count2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == cand1)
+            count1++;
+        else if (nums[i] == cand2)
+            count2++;
+        else if (count1 == 0)
+        {
+            cand1 = nums[i];
+            count1 = 1;
+        }
+        else if (count2 == 0)
+        {
+            cand2 = nums[i];
+            count2 = 1;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = 0;
+    count2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == cand1)
+            count1++;
+        else if (nums[i] == cand2)
+            count2++;
+    }
+
+    vector<int> res;
+
+    if (count1 > n / 3)
+        res.push_back(cand1);
+    if (count2 > n / 3)
+        res.push_back(cand2);
+
+    return res;
+}
 int main()
 {
     return 0;
