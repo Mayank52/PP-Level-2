8f08750a0da48f7d21ffe8962b54e580930fb0aa
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-14 13:14:13 +0530

    Arrays update

--- Arrays.cpp
+++ Arrays.cpp
@@ -1717,6 +1717,34 @@ vector<vector<int>> intervalIntersection(vector<vector<int>> &firstList, vector<
     return res;
 }
 
+// 57. Insert Interval
+vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+{
+    int n = intervals.size();
+    vector<vector<int>> res;
+
+    int i = 0;
+    //add all intervals less than the new interval to res i.e. whose end < newInterval Start
+    while (i < n && intervals[i][1] < newInterval[0])
+        res.push_back(intervals[i++]);
+
+    //merge until current interval <= new interval end
+    vector<int> mergedInterval = newInterval;
+    while (i < n && intervals[i][0] <= newInterval[1])
+    {
+        mergedInterval[0] = min(intervals[i][0], mergedInterval[0]);
+        mergedInterval[1] = max(intervals[i][1], mergedInterval[1]);
+        i++;
+    }
+    res.push_back(mergedInterval);
+
+    //add remaining into res
+    while (i < n)
+        res.push_back(intervals[i++]);
+
+    return res;
+}
+
 int main()
 {
     return 0;
