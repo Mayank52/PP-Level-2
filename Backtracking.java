5ae4548ec8ff1a842c2e33a49ac686d3c0083ea9
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-22 20:43:38 +0530

    Backtracking update

--- Backtracking.java
+++ Backtracking.java
@@ -139,6 +139,182 @@ public class Backtracking {
         queensPermutations(qpsf, tq, row, col, asf + '-' + smallAns, queens);
     }
 
+    // Queens Combinations - 2d As 2d - Queen Chooses
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int i, int j) {
+        if (qpsf == tq) {
+            for (int row = 0; row < tq; row++) {
+                for (int col = 0; col < tq; col++) {
+                    if (chess[row][col])
+                        System.out.print("q\t");
+                    else
+                        System.out.print("-\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            return;
+        }
+
+        // start column from next column of the first row of current step
+        // otherwise column starts from 0
+        // because we need to iterate on elements after the last element {i,j}
+        for (int row = i; row < tq; row++) {
+            for (int col = (row == i) ? (j + 1) : 0; col < tq; col++) {
+                if (!chess[row][col]) {
+                    chess[row][col] = true;
+                    queensCombinations(qpsf + 1, tq, chess, row, col);
+                    chess[row][col] = false;
+                }
+            }
+        }
+    }
+
+    // Queens Permutations - 2d As 2d - Queen Chooses
+    public static void queensPermutations(int qpsf, int tq, int[][] chess) {
+        if (qpsf == tq) {
+            for (int row = 0; row < tq; row++) {
+                for (int col = 0; col < tq; col++) {
+                    if (chess[row][col] != 0)
+                        System.out.print("q" + chess[row][col] + "\t");
+                    else
+                        System.out.print("-\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            return;
+        }
+
+        for (int row = 0; row < tq; row++) {
+            for (int col = 0; col < tq; col++) {
+                if (chess[row][col] == 0) {
+                    chess[row][col] = qpsf + 1;
+                    queensPermutations(qpsf + 1, tq, chess);
+                    chess[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    // Queens Combinations - 2d As 1d - Queen Chooses
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int lcno) {
+        if (qpsf == tq) {
+            for (int row = 0; row < tq; row++) {
+                for (int col = 0; col < tq; col++) {
+                    if (chess[row][col])
+                        System.out.print("q\t");
+                    else
+                        System.out.print("-\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            return;
+        }
+
+        int i = lcno / tq;
+        int j = lcno % tq;
+        for (int row = i; row < tq; row++) {
+            for (int col = (row == i) ? (j + 1) : 0; col < tq; col++) {
+                if (!chess[row][col]) {
+                    chess[row][col] = true;
+                    queensCombinations(qpsf + 1, tq, chess, row * tq + col);
+                    chess[row][col] = false;
+                }
+            }
+        }
+    }
+
+    // Nqueens Combinations - 2d As 1d - Queen Chooses
+    public static boolean IsQueenSafe(boolean[][] chess, int row, int col) {
+        // row
+        for (int i = 0; i < col; i++) {
+            if (chess[row][i])
+                return false;
+        }
+
+        // col
+        for (int i = 0; i < row; i++)
+            if (chess[i][col])
+                return false;
+
+        // diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (chess[i][j])
+                return false;
+
+        // anti diagonal
+        for (int i = row, j = col; i >= 0 && j < chess[0].length; i--, j++)
+            if (chess[i][j])
+                return false;
+
+        return true;
+    }
+
+    // Nqueens Permutations - 2d As 1d - Queen Chooses
+    public static boolean IsQueenSafe(int[][] chess, int row, int col) {
+        // row
+        for (int i = 0; i < chess.length; i++) {
+            if (chess[row][i] != 0)
+                return false;
+        }
+
+        // col
+        for (int i = 0; i < chess.length; i++)
+            if (chess[i][col] != 0)
+                return false;
+
+        // diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (chess[i][j] != 0)
+                return false;
+        for (int i = row, j = col; i < chess.length && j < chess[0].length; i++, j++)
+            if (chess[i][j] != 0)
+                return false;
+
+        // anti diagonal
+        for (int i = row, j = col; i >= 0 && j < chess[0].length; i--, j++)
+            if (chess[i][j] != 0)
+                return false;
+        for (int i = row, j = col; j >= 0 && i < chess.length; i++, j--)
+            if (chess[i][j] != 0)
+                return false;
+
+        return true;
+    }
+
+    public static void nqueens(int qpsf, int tq, int[][] chess) {
+        // write your code here
+        if (qpsf == tq) {
+            for (int i = 0; i < chess.length; i++) {
+                for (int j = 0; j < chess[0].length; j++) {
+                    if (chess[i][j] != 0)
+                        System.out.print("q" + chess[i][j] + "\t");
+                    else
+                        System.out.print("-" + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            return;
+        }
+
+        for (int i = 0; i < chess.length * chess.length; i++) {
+            int row = i / chess.length;
+            int col = i % chess.length;
+
+            if (chess[row][col] == 0 && IsQueenSafe(chess, row, col)) {
+                chess[row][col] = qpsf + 1;
+                nqueens(qpsf + 1, tq, chess);
+                chess[row][col] = 0;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int nboxes = Integer.parseInt(br.readLine());
