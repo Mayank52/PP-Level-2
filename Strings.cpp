e410d6d06c1f3e372043635f8246662b286a8db5
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-24 12:30:11 +0530

    Strings update

--- Strings.cpp
+++ Strings.cpp
@@ -1,13 +1,106 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
+// 1392. Longest Happy Prefix (LPS)
+/*
+Approach: O(n), O(n), (Standard Algo)
+A string is a proper prefix, if it is also a suffix
+Eg: aabaacaabaad
+Here, lps is aabaa, as it a prefix as well as a suffix
+We see the suffix from left. Like in 'leetcodeleet', lps is 'leet'
+*/
+string longestPrefix(string str)
+{
+    int n = str.size();
+    vector<int> lps(n);
+    int len = 0, i = 1;
 
+    while (i < n)
+    {
+        if (str[i] == str[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len > 0)
+                len = lps[len - 1];
+            else
+                i++;
+        }
+    }
+
+    string res = str.substr(0, len);
+
+    return res;
+}
+
+// 214. Shortest Palindrome
+/*
+Approach: O(n), O(n)
+Using KMP, which is an application of LPS
+
+A String is palindrome if after reversing it, it remains the same
+So, use lps to find the longest substring in the given string which is a palindrome
+
+Make a string str = s + # + reverse(s)
+Use LPS, it returns the longest palindrome length of the string s from index 0,
+now we will take the remaining substring, reverse it and append it to start of string s
+
+Eg: aabaacde
+Str = aabaacde#edcaabaa
+
+Lps finds string aabaa of length 5
+Now, the smallest string to be added is of length size(s) - len = 8 - 5 = 3
+which is cde
+So ans is reverse of cde + given string s
+This is the shortest palindrome
+*/
+string shortestPalindrome(string s)
+{
+    //Make string = s + # + reverse(s)
+    string revStr = s;
+    reverse(revStr.begin(), revStr.end());
+    string str = s + '#' + revStr;
+
+    //LPS
+    int n = str.size();
+    vector<int> lps(n);
+    int len = 0, i = 1;
+
+    while (i < n)
+    {
+        if (str[i] == str[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len > 0)
+                len = lps[len - 1];
+            else
+                i++;
+        }
+    }
+
+    //Get the substring to be added, reverse it
+    string res = str.substr(len, s.size() - len);
+    reverse(res.begin(), res.end());
+
+    //add it to beginning and return
+    return res + s;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
 
     return 0;
 }
--- Strings.java
+++ Strings.java
@@ -3,14 +3,49 @@ import java.util.*;
 
 public class Strings {
 
+    // Longest Prefix that is also a Suffix (LPS)
+    // 1392. Longest Happy Prefix
+    /*
+        Approach: O(n), O(n)
+        A string is a proper prefix, if it is also a suffix
+        Eg: aabaacaabaad
+        Here, lps is aabaa, as it a prefix as well as a suffix
+        We see the suffix from left. Like in 'leetcodeleet', lps is 'leet'
+    */
+    public String longestPrefix(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0, i = 1;
+
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else
+                    i++;
+            }
+        }
+
+        String res = str.substring(0, len);
+
+        return res;
+
+    }
+
     // Kmp Pattern Searching
     /*
-     * Approach: O(n) Use the LPS Algo. Make the string = pattern#text Whenever the
-     * len is equal to pattern length, we have found an occurence of the pattern To
-     * get its start index, we have to do start index = i - (pat.length() + 1) -
-     * pat.length() + 1 Where, (pat.length() + 1) = length of the pattern + # we
-     * added to txt and - pat.length() + 1 to get the start index from the last
-     * index
+     * Approach: O(n), O(n)
+     * Use the LPS Algo. 
+     * Make the string = pattern#text.
+     * Whenever the len is equal to pattern length, we have found an occurence of the pattern
+     * To get its start index, we have to do start index = i - (pat.length() + 1) - pat.length() + 1
+     * Where, (pat.length() + 1) = (length of the pattern + #) we added to txt
+     * and - pat.length() + 1 to get the start index from the last index
      */
     public static void lps(String txt, String pat) {
         String str = pat + "#" + txt;
@@ -43,6 +78,8 @@ public class Strings {
         lps(txt, pat);
         System.out.println();
     }
+
+    
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
