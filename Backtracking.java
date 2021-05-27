e2779567578012bbb0a9212fbac7b2a70c6c8fe1
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-27 13:05:45 +0530

    Backtracking update

--- Backtracking.java
+++ Backtracking.java
@@ -971,6 +971,54 @@ public class Backtracking {
 
     }
 
+    // Cryptarithmetic
+    public static int getNum(String str, HashMap<Character, Integer> charIntMap) {
+        int num = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int d = charIntMap.get(ch);
+            num = num * 10 + d;
+        }
+
+        return num;
+    }
+
+    public static boolean isValid(String s1, String s2, String s3, HashMap<Character, Integer> charIntMap) {
+        int num1 = getNum(s1, charIntMap);
+        int num2 = getNum(s2, charIntMap);
+        int num3 = getNum(s3, charIntMap);
+
+        return (num1 + num2) == num3;
+    }
+
+    public static void crypto(String unique, int idx, HashMap<Character, Integer> charIntMap, boolean[] usedNumbers,
+            String s1, String s2, String s3) {
+        if (idx == unique.length()) {
+            if (isValid(s1, s2, s3, charIntMap)) {
+                char[] charArr = unique.toCharArray();
+                Arrays.sort(charArr);
+                for (char ch : charArr) {
+                    int n = charIntMap.get(ch);
+                    System.out.print(ch + "-" + n + " ");
+                }
+                System.out.println();
+            }
+
+            return;
+        }
+
+        char ch = unique.charAt(idx);
+        for (int i = 0; i < usedNumbers.length; i++) {
+            if (!usedNumbers[i]) {
+                usedNumbers[i] = true;
+                charIntMap.put(ch, i);
+                crypto(unique, idx + 1, charIntMap, usedNumbers, s1, s2, s3);
+                charIntMap.put(ch, -1);
+                usedNumbers[i] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
     }
 
