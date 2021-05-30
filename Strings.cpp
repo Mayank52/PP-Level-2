8857fe94ceff868235314beda49f84e889fa2032
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-30 18:33:04 +0530

    Strings update

--- Strings.cpp
+++ Strings.cpp
@@ -145,6 +145,82 @@ void zAlgo(string txt, string pat)
     }
 }
 
+//www.codechef.com/COOK103B/problems/SECPASS
+/*
+Approach: Zalgo O(n)
+Every time you find a prefix as a substring you update the count of that prefix length
+Eg: if length 3 prefix was found as subtring
+That means count of length 3,2,1 will be increased by 1 because
+that 3 length will always have the smaller lengths included in it.
+So the prefix that occurs as a substring max times will always be the smallest non zero value of Z Array.
+
+EG: 
+a b c a b a b a b
+0 0 0 2 0 2 0 2 0
+Here ans is 2
+
+a b c a b c a a b
+0 0 0 3 0 0 1 2
+Here ans is 1
+As the pefix abc which occurs as a substring also contains prefix a,ab
+And for prefix ab which has length 2, contains, a
+So count of prefix lengths will be
+3 - 1
+2 - 1 + 1
+1 - 1 + 1 + 1 (max)
+
+So ans is 1 mostly, as it is included in all prefixes
+Except, if the value of Z Array is same evrywhere, as in eg 1, where all non zero values are 2
+*/
+int chefAndSecretPassword_(string &str)
+{
+    int n = str.length();
+
+    vector<int> zarr(n);
+
+    int i = 1;
+    int l = 0, r = 0;
+    int minLen = n;
+
+    while (i < n)
+    {
+        if (i <= r)
+            zarr[i] = min(r - i + 1, zarr[i - l]);
+
+        while (i + zarr[i] < n && str[i + zarr[i]] == str[zarr[i]])
+            zarr[i]++;
+
+        if (i + zarr[i] - 1 > r)
+        {
+            l = i;
+            r = i + zarr[i] - 1;
+        }
+
+        if (zarr[i] > 0)
+            minLen = min(minLen, zarr[i]);
+
+        i++;
+    }
+
+    return minLen;
+}
+void chefAndSecretPassword()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+
+        int len = chefAndSecretPassword_(str);
+        string res = str.substr(0, len);
+        cout << res << endl;
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
