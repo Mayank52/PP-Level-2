8dccc48a71581cab025ec0ec103d1dc520b9b1e6
Mayank52 <mayank.aggarwal963@gmail.com>
2021-06-07 20:08:53 +0530

    Trees update

--- Trees.cpp
+++ Trees.cpp
@@ -62,29 +62,59 @@ vector<int> diagonal(Node *root)
 }
 
 // 968. Binary Tree Cameras
+/*
+Approach: O(n)
+
+We can have 3 kinds of nodes:
+1. Leaf Node -> 0
+2. Parent of Leaf Node -> 1
+3. Every Other node which does not have a camera but is observed by some other camera -> 2
+
+We never place a camera on leaf. Because placing a camera on parent of leaf is always better as we parent can cover more nodes.
+So, always place a camera on parent of leaf.
+
+So, if the child returns:
+0-> they are leaf, so I am parent, place a camera here
+1-> they are parent of leaf, so they have a camera, which , means I am already covered by my child.
+2-> My child does not have a camera, but is covered by another camera.
+
+
+So, apart from leaf nodes and their parents, for all other nodes, 
+if both their children are already covered by another camera(returns 2)
+Then this node does not have any effect on its children and the children have no effect on this node.
+So, This node can be treated as a leaf node. So, it returns 0.
+
+One Edge case is if root is a leaf, or being treated as a leaf. That can be checked in the main function call.
+*/
 int cameras = 0;
 int minCameraCover_(TreeNode *root)
 {
+    //have no affect on my parent
     if (root == nullptr)
         return 2;
 
     int leftAns = minCameraCover_(root->left);
     int rightAns = minCameraCover_(root->right);
 
-    if (leftAns == 0 || rightAns == 0) //has a leaf node as child -> place camera
+    //has a leaf node as child -> place camera
+    if (leftAns == 0 || rightAns == 0)
     {
         cameras++;
         return 1;
     }
-    else if (leftAns == 1 || rightAns == 1) //I am parent of a leaf node
+    //I am parent of a leaf node
+    else if (leftAns == 1 || rightAns == 1)
         return 2;
-    else //My children have no affect on meF
+    //My children have no affect on me
+    else
         return 0;
 }
 int minCameraCover(TreeNode *root)
 {
     int rootAns = minCameraCover_(root);
-    if (rootAns == 0) //root is leaf or is not affected by its children -> place camera on root
+
+    //root is leaf or is not affected by its children -> place camera on root
+    if (rootAns == 0)
         cameras++;
 
     return cameras;
