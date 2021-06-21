21d85167383473803e3152ca523ff1d2ef873de0
Mayank52 <mayank.aggarwal963@gmail.com>
2021-06-21 18:46:14 +0530

    update

--- AVLTree.java
+++ AVLTree.java
@@ -29,7 +29,6 @@ class AVLTree {
 
         return B;
     }
-
     public Node rr(Node root) {
         if (root == null)
             return null;
@@ -45,8 +44,6 @@ class AVLTree {
 
         return B;
     }
-
-    // While checking rotation check LR before LL and RL before RR
     public Node checkRotation(Node root) {
         if (root == null)
             return null;
@@ -79,21 +76,18 @@ class AVLTree {
 
         return root;
     }
-
     public int height(Node node) {
         if (node == null)
             return 0;
 
         return node.height;
     }
-
     public void updateHeight(Node node) {
         int lh = height(node.left);
         int rh = height(node.right);
 
         node.height = Math.max(lh, rh) + 1;
     }
-
     public Node insertToAVL(Node node, int data) {
         if (node == null)
             return new Node(data);
@@ -108,6 +102,31 @@ class AVLTree {
 
     // AVL Tree Deletion
     // https://practice.geeksforgeeks.org/problems/avl-tree-deletion/1
+    /*
+    While deleting, when you delete a node, you may get a balance factor of 2 or -2
+    But the balance of left and right child may be 0.
+    So, You have to use LL or RR in two cases:
+        1. Balance factor = 1(RR) or -1(LL)
+        2. Balance factor = 0
+
+    For Eg:
+            4
+           / \
+          2   10
+             /  \
+            7    11
+           /      \
+          6        12
+
+    Here we have to call RR(node) for node 4. The balance factor of 4 is -2
+    But the balance factor of its right child is 0. We have to do RR rotation for it.
+    So, we have 2 cases for RR: 0 and -1
+    Similarly for LL 2 cases are : 0 and 1
+
+    This situation does not occur while adding a node because it will be balanced already before
+    it reaches this arrangement. Because it will already be unbalanced when the first node is added to the last level.
+    
+    */
     public Node deleteNode_(Node root, int key) {
         if (root == null)
             return null;
