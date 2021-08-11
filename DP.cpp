af23e71e57257da09253018c2edfc6f04e72bc15
Mayank52 <mayank.aggarwal963@gmail.com>
2021-08-11 21:26:58 +0530

    DP update

--- DP.cpp
+++ DP.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+// LIS======================================================================================
 // 300. Longest Increasing Subsequence
 /*
 Approach 1: O(n^2)
@@ -68,7 +70,7 @@ int lengthOfLIS(vector<int> &nums)
 
             if (nums[i] > dp[mid])
                 lo = mid + 1;
-            else
+            else // this handles <= case
                 hi = mid;
         }
 
@@ -82,12 +84,11 @@ int lengthOfLIS(vector<int> &nums)
 }
 
 // 673. Number of Longest Increasing Subsequence
-
 int findNumberOfLIS(vector<int> &nums)
 {
     int n = nums.size();
 
-    vector<pair<int, int>> dp(n, {1, 1});   // { length, count of subseq with that length}
+    vector<pair<int, int>> dp(n, {1, 1}); // { length, count of subseq with that length}
     int res = 0, count = 0;
 
     for (int i = 0; i < n; i++)
@@ -96,8 +97,10 @@ int findNumberOfLIS(vector<int> &nums)
         {
             if (nums[i] > nums[j])
             {
+                // if the val at j == val at i, then add the count of its increasing subseq to current element's count
                 if (dp[i].first == dp[j].first + 1)
                     dp[i].second += dp[j].second;
+                // else the count will be same as the element in whose subseq, this element is being added
                 else if (dp[i].first < dp[j].first + 1)
                 {
                     dp[i].first = dp[j].first + 1;
@@ -106,16 +109,187 @@ int findNumberOfLIS(vector<int> &nums)
             }
         }
 
+        // update the res if current element has longer subseq
         if (dp[i].first > res)
         {
             res = dp[i].first;
             count = dp[i].second;
         }
+        // else if the lenght is same, then this count to the previous count to get the total count of this length
         else if (dp[i].first == res)
             count += dp[i].second;
     }
 
     return count;
+}
+
+// Building Bridges (SPOJ)(https://www.spoj.com/problems/BRDGHRD/)
+/*
+Approach: LIS O(nlogn)
+Store both arrays in a single nx2 array
+Then sort this array with respect to one bridge points i.e 0th index or 1st index
+Then for the other bridge points find the LIS
+This LIS is the answer
+*/
+void buildingBridges()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> arr(n, vector<int>(2));
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i][0];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i][1];
+
+        sort(arr.begin(), arr.end());
+
+        vector<int> dp;
+        int res = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int lo = 0, hi = dp.size();
+            while (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (arr[i][1] < dp[mid])
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+
+            if (lo == dp.size())
+                dp.push_back(arr[i][1]);
+            else
+                dp[lo] = arr[i][1];
+        }
+
+        cout << dp.size() << '\n';
+    }
+}
+
+// 354. Russian Doll Envelopes
+/*
+Approach: LIS
+Sort the array on basis of increasing height and decreasing width
+This way LIS wont add elements with increasing width and equal height to same subseq
+Because the width of consecutive equal heights will be decreasing
+Eg:
+(3, 5), (4, 6), (4, 5), (4, 4)
+Here as the height is equal to 4 for 3 consecutive
+So, if width is also increasing then in LIS we will have to check for both height and width
+But if we make width decreasing, then we only need to check for width
+
+*/
+// Approach 1: O(n^2)
+int maxEnvelopes(vector<vector<int>> &envelopes)
+{
+    // sort only in increasing order
+    sort(envelopes.begin(), envelopes.end());
+
+    int n = envelopes.size();
+
+    vector<int> dp(n, 1);
+    int res = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+            // check for both width and height
+            if (envelopes[i][1] > envelopes[j][1] && envelopes[i][0] > envelopes[j][0])
+                dp[i] = max(dp[i], dp[j] + 1);
+
+        res = max(res, dp[i]);
+    }
+
+    return res;
+}
+// Approach 1: O(nlogn)
+int maxEnvelopes(vector<vector<int>> &envelopes)
+{
+    // sort in increasing order of height, and decreasing width for equal heights
+    sort(envelopes.begin(), envelopes.end(), [](vector<int> &v1, vector<int> &v2)
+         {
+             if (v1[0] != v2[0])
+                 return v1[0] < v2[0];
+             else
+                 return v1[1] > v2[1];
+         });
+
+    vector<int> dp;
+
+    // LIS for just widths
+    for (vector<int> &env : envelopes)
+    {
+        int lo = 0, hi = dp.size();
+
+        while (lo < hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            if (env[1] > dp[mid])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+
+        if (lo == dp.size())
+            dp.push_back(env[1]);
+        else
+            dp[lo] = env[1];
+    }
+
+    return dp.size();
+}
+
+
+// Minimum number of increasing subsequences
+/*
+Approach: LIS
+https://cp-algorithms.com/sequences/longest_increasing_subsequence.html
+
+Minimum number of increasing subsequences = length of longest decreasing subsequence
+
+Explanation:
+Every element in the longest decreasing subsequence will belong a seperate increasing subseq
+As if they put in same group they will be in decreasing order
+
+So, Minimum number of increasing subsequences = length of longest decreasing subsequence
+
+*/
+int minNumberofIncSubseq(vector<int> &nums)
+{
+    vector<int> dp;
+
+    // LIS from the n - 1 to 0 index will be LDS from 0 to n - 1
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        // binary search to find the smallest element equal to or just greater than current element
+        int lo = 0, hi = dp.size();
+        while (lo < hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[i] > dp[mid])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+
+        if (lo == dp.size())
+            dp.push_back(nums[i]);
+        else
+            dp[lo] = nums[i];
+    }
+
+    return dp.size();
 }
 
 int main()
