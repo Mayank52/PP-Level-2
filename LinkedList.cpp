d636913fc26436b637dc97f0a155695072fd7666
Mayank52 <mayank.aggarwal963@gmail.com>
2021-09-08 13:27:32 +0530

    LL update

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -219,30 +219,134 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
 }
 
 // 146. LRU Cache
-class DLLNode
-{
-    int val;
-    DLLNode *next;
-    DLLNode *prev;
+/*
+Approach: O(1) operations
+We use a Doubly linked list to maintain the nodes in the order of least recently used
+And a hashmap to maintain {key : address} of each node currently present in the list
+so that we can access them in O(1) from the list. 
 
-    DLLNode(int val)
+get(int key): O(1)
+When we need to get a key from the cache,
+We check if it is present in the hashmap.
+If it is present then we remove it from its current position in list, and 
+put it at the end of the list as it is the most recently used now.
+If it is not present then just return -1.
+
+put(int key, int value): O(1)
+To put a new element, we check if it is already present
+If it is already present, then:
+    1. Update its value
+    2. Make it most recently used: Remove it from its current position in list, and put in at the end of list.
+If it is not present:
+    1. Check if the capacity of list is full, we can check the size of hashmap for this
+       If capacity is full, then remove the first element of list i.e. the least recently used node.
+       So, remove the last node of list, and remove it from the hashmap as well.
+    2. Then we make a new node, and add it to the end of list, and put it in the hashmap
+
+In the code, I have made a dummy head and tail for the doubly linked list to avoid all edge cases
+while adding and removing nodes. So, the actual first node is the head->next node and the actual 
+last node is tail->prev node. We never change the dummy head and tail.
+*/
+class Node
+{
+public:
+    int key;
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val, int key)
     {
         this->val = val;
+        this->key = key;
         next = nullptr;
         prev = nullptr;
     }
 };
 class LRUCache
 {
-    DLLNode* head;
-    int size;
-    unordered_map<int, DLLNode*> mp;
+private:
+    Node *head, *tail; // head is oldest, tail is latest
+    int capacity;
+    unordered_map<int, Node *> mp; //{key: node}
+
+    void addLast(Node *node)
+    {
+        // add a node just before the dummy tail
+        node->next = tail;
+        node->prev = tail->prev;
+
+        tail->prev->next = node;
+        tail->prev = node;
+    }
+    void removeNode(Node *node)
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
 
 public:
     LRUCache(int capacity)
     {
-        size = capacity;
-        head = nullptr;
+        this->capacity = capacity;
+
+        // make a dummy head and tail, so that we dont have to handle edge cases for the linked list
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key)
+    {
+        // if node is not present
+        if (mp.find(key) == mp.end())
+            return -1;
+
+        // else remove it from its current position in list, and add at the end
+        Node *node = mp[key];
+
+        removeNode(node);
+        addLast(node);
+
+        return node->val;
+    }
+
+    void put(int key, int value)
+    {
+        // if already present
+        if (mp.find(key) != mp.end())
+        {
+            // remove it from its current position in list, and add at the end
+            Node *node = mp[key];
+
+            node->val = value; // update its value
+            removeNode(node);
+            addLast(node);
+        }
+        else
+        { // if size is full, then remove the first node
+            if (mp.size() == capacity)
+            {
+                mp.erase(head->next->key);
+                removeNode(head->next);
+            }
+
+            // add the node to end of the list, and into the map
+            Node *node = new Node(value, key);
+            addLast(node);
+            mp[key] = node;
+        }
+    }
+};
+
+// 460. LFU Cache
+class LFUCache
+{
+public:
+    LFUCache(int capacity)
+    {
     }
 
     int get(int key)
